d7b2f2d6de97f4441cce4435ec847db50dc7fa17
pan93412 <pan93412@gmail.com>
2019-08-04 18:03:54 +0800

    API: v1.1.0: 修正顯示 Failed 的問題，並解決更多跳脫問題。

--- api/db.php
+++ api/db.php
@@ -6,23 +6,33 @@ class db extends mysqli {
     }
   }
 
-  function insert($name, $desc, $type, $amount, $date) {
-    if ($type === "") $type = "其他";
-    $this->checkPass($this->query(
-      $this->real_escape_string(
+  function escape($string) {
+    return $this->real_escape_string(htmlspecialchars($string, ENT_NOQUOTES));
+  }
+
+  function insert($raw_name, $raw_desc, $raw_type, $raw_amount, $raw_date) {
+    if ($raw_type === "") $raw_type = "其他";
+    
+    $name = $this->escape($raw_name);
+    $desc = $this->escape($raw_desc);
+    $type = $this->escape($raw_type);
+    $amount = $this->escape($raw_amount);
+    $date = $this->escape($raw_date);
+
+    $this->checkPass(
+      $this->query(
         "INSERT INTO journal (`name`, `desc`, `type`, amount, `date`) VALUES ('{$name}', '{$desc}', '{$type}', {$amount}, '{$date}')"
       )
-    ));
+    );
 
     echo "OK"; // 告訴 JS 已經完成
   }
 
   function modify($id, $name, $desc, $type, $amount, $date) {
-    function modifyTool($fieldName, $fieldVal, $that, $id) {
+    function modifyTool($fieldName, $fieldRawVal, $that, $id) {
+      $fieldVal = $that->escape($fieldRawVal);
       if ($fieldVal !== "") $that->checkPass($that->query(
-        $that->real_escape_string(
           "UPDATE journal SET `{$fieldName}`='{$fieldVal}' WHERE `id`={$id}"
-        )
       ));
     }
     
@@ -36,13 +46,13 @@ class db extends mysqli {
   }
   
   function delete($id) {
-    $this->checkPass($this->query($this->real_escape_string("DELETE FROM journal WHERE `id`={$id}")));
+    $this->checkPass($this->query("DELETE FROM journal WHERE `id`={$id}"));
 
     echo "OK"; // 告訴 JS 已經完成
   }
 
   function getData() {
-    $queryResult = $this->query($this->real_escape_string("SELECT * FROM journal"));
+    $queryResult = $this->query("SELECT * FROM journal");
     $result = $queryResult->fetch_all(MYSQLI_ASSOC);
     return json_encode($result, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
   }
@@ -50,7 +60,7 @@ class db extends mysqli {
 
 class loginDB extends mysqli {
   function getUsers() {
-    $queryResult = $this->query($this->real_escape_string("SELECT * FROM login"));
+    $queryResult = $this->query("SELECT * FROM login");
     return $queryResult->fetch_all(MYSQLI_ASSOC);
   }
 }
