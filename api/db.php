83c818c3b7b8eaaf2eeaaf6108cc51ebac48d091
pan93412 <pan93412@gmail.com>
2019-08-04 17:10:06 +0800

    API v1.1.0: 防注入修正

--- api/db.php
+++ api/db.php
@@ -8,14 +8,22 @@ class db extends mysqli {
 
   function insert($name, $desc, $type, $amount, $date) {
     if ($type === "") $type = "其他";
-    $this->checkPass($this->query("INSERT INTO journal (`name`, `desc`, `type`, amount, `date`) VALUES ('{$name}', '{$desc}', '{$type}', {$amount}, '{$date}')"));
+    $this->checkPass($this->query(
+      $this->real_escape_string(
+        "INSERT INTO journal (`name`, `desc`, `type`, amount, `date`) VALUES ('{$name}', '{$desc}', '{$type}', {$amount}, '{$date}')"
+      )
+    ));
 
     echo "OK"; // 告訴 JS 已經完成
   }
 
   function modify($id, $name, $desc, $type, $amount, $date) {
     function modifyTool($fieldName, $fieldVal, $that, $id) {
-      if ($fieldVal !== "") $that->checkPass($that->query("UPDATE journal SET `{$fieldName}`='{$fieldVal}' WHERE `id`={$id}"));
+      if ($fieldVal !== "") $that->checkPass($that->query(
+        $that->real_escape_string(
+          "UPDATE journal SET `{$fieldName}`='{$fieldVal}' WHERE `id`={$id}"
+        )
+      ));
     }
     
     modifyTool('name', $name, $this, $id);
@@ -28,13 +36,13 @@ class db extends mysqli {
   }
   
   function delete($id) {
-    $this->checkPass($this->query("DELETE FROM journal WHERE `id`={$id}"));
+    $this->checkPass($this->query($this->real_escape_string("DELETE FROM journal WHERE `id`={$id}")));
 
     echo "OK"; // 告訴 JS 已經完成
   }
 
   function getData() {
-    $queryResult = $this->query("SELECT * FROM journal");
+    $queryResult = $this->query($this->real_escape_string("SELECT * FROM journal"));
     $result = $queryResult->fetch_all(MYSQLI_ASSOC);
     return json_encode($result, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
   }
@@ -42,7 +50,7 @@ class db extends mysqli {
 
 class loginDB extends mysqli {
   function getUsers() {
-    $queryResult = $this->query("SELECT * FROM login");
+    $queryResult = $this->query($this->real_escape_string("SELECT * FROM login"));
     return $queryResult->fetch_all(MYSQLI_ASSOC);
   }
 }
