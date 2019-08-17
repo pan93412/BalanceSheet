0d11aba2c4725555eb1762746ee0c92e337331e6
pan93412 <pan93412@gmail.com>
2019-08-18 01:52:39 +0800

    v1.3.0: 使用 Webpack 編譯 Pug。

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { ProgressPlugin } = require('webpack')
 
 module.exports = {
   mode: 'production',
   entry: {
-    publicJS: './src/index.js',
-    publicCSS: './src/css.js',
+    js: './src/index.js',
+    css: './src/css.js',
     money: './src/money.js'
   },
   output: {
-    path: path.resolve(__dirname, 'assets'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
   },
+  plugins: [
+    new ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    })
+  ],
   module: {
     rules: [
       {
@@ -31,6 +41,12 @@ module.exports = {
       {
         test: /\.(?:svg|png|eot|woff2|woff|ttf|jpg)/,
         use: 'file-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader'
+        ]
       }
     ]
   }
