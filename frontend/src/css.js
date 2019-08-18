ce2294a35a75e722a8962346eb4a4045875de81e
pan93412 <pan93412@gmail.com>
2019-08-18 15:17:30 +0800

    v2.0.0: 清理非必要依賴 (1/2)

--- frontend/src/css.js
+++ frontend/src/css.js
@@ -1,6 +1,3 @@
 // CSS
-require('@font/nunito/index.css')
 require('bootstrap/dist/css/bootstrap.min.css')
-require('magnific-popup/dist/magnific-popup.css')
-require('aos')
-require('./style.scss')
+require('./css/style.scss')
